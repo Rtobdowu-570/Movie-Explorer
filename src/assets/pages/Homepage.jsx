a96470a986400888fe950f249dcb47d517ea4e90
Rtobdowu-570 <gcah4451@gmail.com>
2026-02-02 23:11:05 +0100

    Fix the hompage backgroung to change dynamically

--- src/assets/pages/Homepage.jsx
+++ src/assets/pages/Homepage.jsx
@@ -3,18 +3,44 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Vidsection from "../components/Vidsection";
 import Footer from "../components/Footer";
-import "../styles/main.css";
-import image from "../images/image.jpg";
+import { useState, useEffect } from 'react';
+
 
 const Homepage = () => {
+  const [featuredMovie, setFeaturedMovie] = useState(null);
+
+  useEffect(() => {
+    const fetchTrending = async () => {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+        },
+      };
+
+      try {
+        const res = await fetch("https://api.themoviedb.org/3/trending/movie/day", options);
+        const data = await res.json();
+        
+        const randomIdx = Math.floor(Math.random() * data.results.length);
+        setFeaturedMovie(data.results[randomIdx]);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchTrending();
+  }, []);
+
   return (
     <>
       <div
         className="header-background"
-        style={{ backgroundImage: `url(${image})` }}
+        style={{ backgroundImage: `url(${featuredMovie ? `https://image.tmdb.org/t/p/original${featuredMovie.backdrop_path}` : ''})` }}
       >
         <Navbar />
-        <Hero />
+        <Hero movie={featuredMovie} />
       </div>
       <Vidsection />
       <Footer />
